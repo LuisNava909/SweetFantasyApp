be23b6efb792fd0079c4284da6b0468f9d941c5f
Luis Nava <naroluen@gmail.com>
2022-04-17 00:11:52 -0500

    Pantalla de registro finalizada con validación, sin funcionalidad en firebase

---
+++ app/src/main/java/com/gmail/naroluen/sweetfantasy/activities/BaseActivity.kt
@@ -0,0 +1,37 @@
+package com.gmail.naroluen.sweetfantasy.activities
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import com.gmail.naroluen.sweetfantasy.R
+import com.google.android.material.snackbar.Snackbar
+
+open class BaseActivity : AppCompatActivity() {
+
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarError
+                )
+            )
+        }else{
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+        }
+        snackBar.show()
+    }
+
+}
---
+++ app/src/main/java/com/gmail/naroluen/sweetfantasy/activities/RegisterActivity.kt
@@ -0,0 +1,97 @@
+package com.gmail.naroluen.sweetfantasy.activities
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.gmail.naroluen.sweetfantasy.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        setupActionBar()
+
+        tv_login.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+            //finish()
+        }
+
+        btn_register.setOnClickListener{
+            validateRegisterDetails()
+        }
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_register_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24)
+        }
+
+        toolbar_register_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            /*!cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }*/
+            else -> {
+                showErrorSnackBar("Nueva cuenta registrada", false)
+                true
+            }
+        }
+    }
+
+}
