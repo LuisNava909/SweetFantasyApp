6ee92cdfe85dee1f49309ccbd858de7f124087be
Luis Nava <naroluen@gmail.com>
2022-05-18 13:40:24 -0500

    Agregar y recuperar direcciones - 100

--- app/src/main/java/com/gmail/naroluen/sweetfantasy/ui/activities/AddressListActivity.kt
+++ app/src/main/java/com/gmail/naroluen/sweetfantasy/ui/activities/AddressListActivity.kt
@@ -3,17 +3,57 @@ package com.gmail.naroluen.sweetfantasy.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gmail.naroluen.sweetfantasy.R
+import com.gmail.naroluen.sweetfantasy.firestore.FirestoreClass
+import com.gmail.naroluen.sweetfantasy.model.Address
+import com.gmail.naroluen.sweetfantasy.ui.adapters.AddressListAdapter
 import kotlinx.android.synthetic.main.activity_address_list.*
 
-class AddressListActivity : AppCompatActivity() {
+class AddressListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_address_list)
         setupActionBar()
+
         tv_add_address.setOnClickListener {
             val intent = Intent(this@AddressListActivity, AddEditAddressActivity::class.java)
             startActivity(intent)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getAddressList()
+    }
+
+    //et the list of address from cloud firestore.
+    private fun getAddressList() {
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().getAddressesList(this@AddressListActivity)
+    }
+
+    /**
+     * A function to get the success result of address list from cloud firestore.
+     *
+     * @param addressList
+     */
+    fun successAddressListFromFirestore(addressList: ArrayList<Address>) {
+        hideProgressDialog()
+        //Populate the address list in the UI.
+        if (addressList.size > 0) {
+            rv_address_list.visibility = View.VISIBLE
+            tv_no_address_found.visibility = View.GONE
+            rv_address_list.layoutManager = LinearLayoutManager(this@AddressListActivity)
+            rv_address_list.setHasFixedSize(true)
+            val addressAdapter = AddressListAdapter(this@AddressListActivity, addressList)
+            rv_address_list.adapter = addressAdapter
+        } else {
+            rv_address_list.visibility = View.GONE
+            tv_no_address_found.visibility = View.VISIBLE
         }
     }
 
